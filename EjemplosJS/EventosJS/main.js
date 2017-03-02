64974ee046ab0a74c589b70c5b693d947c69ab77
LuceroG <ml17ft@gmail.com>
2017-03-02 12:03:18 -0600

    eventos

---
+++ EjemplosJS/EventosJS/main.js
@@ -0,0 +1,11 @@
+function saludar(){
+    alert('hola')
+}
+function saludar2()
+{
+    document.write('hola documento')
+}
+var btnSaluda = document.getElementById('btn-saluda')
+btnSaluda.onclick = saludar
+
+var btnSaluda2 = document.getElementById('btn-saluda-v2')
